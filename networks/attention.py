b1d34ffeb0da0e3bafea79b72ef9243820a7d9ec
yuto425 <yuto25@gmail.com>
2019-09-26 19:29:51 +0800

    Create multi headed attention

--- networks/attention.py
+++ networks/attention.py
@@ -27,3 +27,49 @@ class SimpleAttention(nn.Module):
         attention_output = torch.matmul(normalized_weights, value)
         output = self._out(attention_output)
         return output
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self,
+                 depth: int,
+                 head_num: int,
+                 dropout_rate: float = 0.1) -> None:
+        super().__init__()
+        self._query_linear = nn.Linear(depth, depth)
+        self._key_linear = nn.Linear(depth, depth)
+        self._value_linear = nn.Linear(depth, depth)
+        self._out = nn.Linear(depth, depth)
+        self._depth = depth
+        self._head_num = head_num
+        assert self._depth % self._head_num == 0
+        self._head_size = int(self._depth / self._head_num)
+        self._dropout = nn.Dropout(dropout_rate)
+
+    def forward(self,
+                input: torch.Tensor,
+                memory: torch.Tensor,
+                mask: torch.Tensor) -> torch.Tensor:
+        mixed_query = self._query_linear(input)
+        mixed_key = self._key_linear(memory)
+        mixed_value = self._value_linear(memory)
+        query = self._split_head(mixed_query)
+        key = self._split_head(mixed_key)
+        value = self._split_head(mixed_value)
+        weights = torch.matmul(
+            query, key.transpose(-1, -2)) / math.sqrt(self._head_size)
+        mask = mask.unsqueeze(1)
+        weights = weights.masked_fill(mask == 0, -1e10)
+        normalized_weights = F.softmax(weights, dim=-1)
+        normalized_weights = self._dropout(normalized_weights)
+        attention_output = torch.matmul(normalized_weights, value)
+        return self._combine_head(attention_output)
+
+    def _split_head(self, x: torch.Tensor) -> torch.Tensor:
+        new_shape = x.size()[:-1] + (self._head_num, self._head_size)
+        x = x.view(*new_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def _combine_head(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.permute(0, 2, 1, 3).contiguous()
+        new_shape = x.size()[:-2] + (self._depth,)
+        return x.view(*new_shape)
--- tests/networks/attention_spec.py
+++ tests/networks/attention_spec.py
@@ -2,7 +2,7 @@ import torch
 import random
 from mamba import description, context, it, before
 from expects import expect, equal
-from networks.attention import SimpleAttention
+from networks.attention import SimpleAttention, MultiHeadedAttention
 
 
 with description('SimpleAttention') as self:
@@ -27,6 +27,34 @@ with description('SimpleAttention') as self:
     with context('when calculate with input, memory, and mask'):
         with it('should return a tensor with expected shape'):
             model = SimpleAttention(self.depth)
+            output = model(self.input, self.memory, self.mask)
+            expect(list(output.size())).to(equal(
+                [self.batch_size, self.input_length, self.depth]))
+
+
+with description('MultiHeadedAttention') as self:
+    with before.each:
+        self.batch_size = 2
+        self.depth = 300
+        self.head_num = 2
+        self.input_length = 5
+        self.memory_length = 3
+        self.input = torch.randn(
+            self.batch_size, self.input_length, self.depth,
+            dtype=torch.float32)
+        self.memory = torch.randn(
+            self.batch_size, self.memory_length, self.depth,
+            dtype=torch.float32)
+        self.mask = torch.tensor(
+            [[[1 if random.random() * self.memory_length > i else 0
+               for i in range(self.memory_length)]
+              for _ in range(self.input_length)]
+             for _ in range(self.batch_size)],
+            dtype=torch.bool)
+
+    with context('when calculate with input, memory, and mask'):
+        with it('should return a tensor with expected shape'):
+            model = MultiHeadedAttention(self.depth, self.head_num)
             output = model(self.input, self.memory, self.mask)
             expect(list(output.size())).to(equal(
                 [self.batch_size, self.input_length, self.depth]))
